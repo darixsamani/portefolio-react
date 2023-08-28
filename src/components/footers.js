797d6a6efe8bed0b84d01b8e7a436dee2f7ff740
samani siewe darix <samanidarix@gmail.com>
2023-08-28 14:50:26 +0100

    add footer css

--- src/components/footers.js
+++ src/components/footers.js
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
 import React from "react";
-
+import "./footers.css"
 function Footer(){
 
 
     return (
         <div className="footers" style={{backgroundColor: "#1e4c7a"}}>
+            
             <p>Design by <Link to={{ pathname: "https://github.com/darixsamani" }} target="_blank" > @darixsamani </Link></p>
 
         </div>
