2643816a157f263efc3273315b83387de9606d9a
samani siewe darix <samanidarix@gmail.com>
2023-08-29 09:26:58 +0100

    add datas.js

---
+++ src/components/datas.js
@@ -0,0 +1,22 @@
+const tools = [
+
+    {
+        "name" : "Programming Languages ",
+        "items": ["python", "JavaScrip", "Golang", "Pharo", "Dart", "C/C++"]
+
+    },
+    {
+        "name": "Web Frameworks",
+        "items" : ["Django", "Express.js", "Flask", "FastAPI", "ReactJs", "Flutter"]
+    },
+    {
+        "name": "Databases",
+        "items": ["PostgreSQL", "MongoDB", "Neo4j", "Redis", "Cassandra"]
+    },
+    {
+        "name": "DevOps",
+        "items": ["Docker", "Kubernetes", "Shell/bash", "CI/CD", "OpenStack"]
+    }
+]
+
+export default tools
