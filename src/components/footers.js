2c66acff58825ab15f955e130fd668250d3f2b2e
samani siewe darix <samanidarix@gmail.com>
2024-01-12 14:38:49 +0100

    add target in footer link

--- src/components/footers.js
+++ src/components/footers.js
@@ -7,7 +7,7 @@ function Footer(){
     return (
         <div className="footers" style={{backgroundColor: "#1e4c7a"}}>
             
-            <p>Design by <Link to={{ pathname: "https://github.com/darixsamani" }} target="_blank" > @darixsamani </Link></p>
+            <p>Design by <a href="https://github.com/darixsamani" target="_blank" rel="noopener noreferrer" > @darixsamani </a></p>
 
         </div>
     )
